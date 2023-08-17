c6ce6b56fc895fc46ba48fcb48a0b5535b827008
Jonathan Pires <jonathan@subscrbe.com>
2023-08-16 23:58:11 -0400

    delete function tested

--- src/app/todo/todo-state.ts
+++ src/app/todo/todo-state.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Action, State, StateContext } from '@ngxs/store';
 import { TodoModel } from './todo';
-import { AddItemAction, ToggleItemAction } from './todo-actions';
+import {
+  AddItemAction,
+  ToggleItemAction,
+  DeleteItemAction,
+  EditItemAction,
+} from './todo-actions';
 
 export interface TodoStateModel {
   items: TodoModel[];
@@ -53,4 +58,18 @@ export class TodoState {
       items: [...newTodoItems],
     });
   }
+
+  @Action(DeleteItemAction)
+  deleteItem(ctx: StateContext<TodoStateModel>, action: DeleteItemAction) {
+    const state = ctx.getState();
+
+    const newTodoItems = state.items.filter((item) => item.id !== action.id);
+
+    ctx.setState({
+      items: [...newTodoItems],
+    });
+  }
+
+  @Action(EditItemAction)
+  editItem(ctx: StateContext<TodoStateModel>, action: EditItemAction) {}
 }
